62bcc67e012bf68d281fa7960ca0409daff90a5e
Dannygee1988 <158570512+Dannygee1988@users.noreply.github.com>
2025-05-27 10:11:35 +0100

    Update gallery route paths

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,7 @@ function App() {
           </Route>
           
           {/* Redirect to login by default */}
-          <Route path="*" element={<Navigate to="/login" replace />} />
+          <Route path="*" element={<Navigate to="/login\" replace />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
