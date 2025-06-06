62c796bdf790aba561d393ca7c671e64ff614fd7
Dannygee1988 <158570512+Dannygee1988@users.noreply.github.com>
2025-06-06 13:57:00 +0200

    Updated CreateSocialPost.tsx

--- src/pages/CreateSocialPost.tsx
+++ src/pages/CreateSocialPost.tsx
@@ -358,8 +358,8 @@ ${includeCallToAction ? '\n👉 Learn more at our website!' : ''}`;
                         includeHashtags ? 'bg-primary border-primary' : 'border-neutral-300'
                       }`}>
                         {includeHashtags && (
-                          <svg className="w-4 h-4 text-white absolute top-0.5 left-0.5" fill="currentColor" viewBox="0 0 20 20">
-                            <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
+                          <svg className="w-4 h-4 text-white absolute top-0.5 left-0.5\" fill="currentColor\" viewBox="0 0 20 20">
+                            <path fillRule="evenodd\" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z\" clipRule="evenodd" />
                           </svg>
                         )}
                       </div>
@@ -382,8 +382,8 @@ ${includeCallToAction ? '\n👉 Learn more at our website!' : ''}`;
                         includeEmojis ? 'bg-primary border-primary' : 'border-neutral-300'
                       }`}>
                         {includeEmojis && (
-                          <svg className="w-4 h-4 text-white absolute top-0.5 left-0.5" fill="currentColor" viewBox="0 0 20 20">
-                            <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
+                          <svg className="w-4 h-4 text-white absolute top-0.5 left-0.5\" fill="currentColor\" viewBox="0 0 20 20">
+                            <path fillRule="evenodd\" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z\" clipRule="evenodd" />
                           </svg>
                         )}
                       </div>
@@ -406,8 +406,8 @@ ${includeCallToAction ? '\n👉 Learn more at our website!' : ''}`;
                         includeCallToAction ? 'bg-primary border-primary' : 'border-neutral-300'
                       }`}>
                         {includeCallToAction && (
-                          <svg className="w-4 h-4 text-white absolute top-0.5 left-0.5" fill="currentColor" viewBox="0 0 20 20">
-                            <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
+                          <svg className="w-4 h-4 text-white absolute top-0.5 left-0.5\" fill="currentColor\" viewBox="0 0 20 20">
+                            <path fillRule="evenodd\" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z\" clipRule="evenodd" />
                           </svg>
                         )}
                       </div>
